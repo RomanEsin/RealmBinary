0561f7053977421d4e5e885bf019871f2a681b3b
Roman Esin <40879468+RomanEsin@users.noreply.github.com>
2022-11-17 01:08:00 +1000

    Create package_template.swift

---
+++ .script/package_template.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.5.0
+import PackageDescription
+
+let package = Package(
+    name: "RealmBinary",
+    platforms: [.iOS(.v12)],
+    products: [
+        .library(
+            name: "Realm",
+            targets: ["Realm"]),
+        .library(
+            name: "RealmSwift",
+            targets: ["RealmSwift"])
+    ],
+    dependencies: [
+    ],
+    targets: [
+        // GENERATE BINARIES
+    ]
+)
