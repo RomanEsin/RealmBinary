39a4d3747fb983485c3dc9b4b3160d1c903b0462
Roman Esin <40879468+RomanEsin@users.noreply.github.com>
2022-11-17 16:54:48 +1000

    Update package.sh

--- .scripts/package.sh
+++ .scripts/package.sh
@@ -115,7 +115,7 @@ set -o pipefail
 
 # Repos
 realm_repo="https://github.com/realm/realm-swift"
-xcframeworks_repo="https://github.com/NikitaKosolapov/RealmBinaries"
+xcframeworks_repo="https://github.com/RomanEsin/RealmBinary"
 
 # Release versions
 latest=$(latest_release_number $realm_repo)
